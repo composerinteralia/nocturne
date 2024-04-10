1520c8763d55862d477ced43f713e92ceb11456e
Daniel Colson <danieljamescolson@gmail.com>
2024-04-10 00:27:19 -0400

    Make result enumerable

--- lib/nocturne/result.rb
+++ lib/nocturne/result.rb
@@ -1,10 +1,16 @@
 class Nocturne
   class Result
+    include Enumerable
+
     attr_reader :fields, :rows
 
     def initialize(fields, rows)
       @fields = fields
       @rows = rows
+    end
+
+    def each(&bk)
+      rows.each(&bk)
     end
   end
 end
