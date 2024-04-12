a275e0df2c3241ffb0c4b13ef1271911f4abc0c8
Daniel Colson <danieljamescolson@gmail.com>
2024-04-11 22:02:21 -0400

    Move query command to a separate class

--- lib/nocturne.rb
+++ lib/nocturne.rb
@@ -4,6 +4,7 @@ require "socket"
 require "digest"
 require_relative "nocturne/error"
 require_relative "nocturne/protocol/connection"
+require_relative "nocturne/protocol/query"
 require_relative "nocturne/read/packet"
 require_relative "nocturne/read/payload"
 require_relative "nocturne/result"
@@ -25,7 +26,9 @@ class Nocturne
   def initialize(options = {})
     @options = options
     @sock = Nocturne::Socket.new(options)
-    connect
+
+    connection = Protocol::Connection.new(@sock, @options).tap(&:establish)
+    @server_version = connection.server_version
   end
 
   def change_db(db)
@@ -42,58 +45,7 @@ class Nocturne
   alias_method :select_db, :change_db
 
   def query(sql)
-    @sock.begin_command
-
-    @sock.write_packet do |packet|
-      packet.int(1, COM_QUERY)
-      packet.str(sql)
-    end
-
-    column_count = 0
-    @sock.read_packet do |payload|
-      unless payload.ok?
-        column_count = payload.lenenc_int
-      end
-    end
-
-    fields = []
-    rows = []
-    if column_count > 0
-      column_count.times do
-        @sock.read_packet do |column|
-          column.lenenc_str
-          column.lenenc_str
-          column.lenenc_str
-          column.lenenc_str
-          column.lenenc_str
-          name = column.lenenc_str
-          column.lenenc_int
-          column.int(2)
-          column.int(4)
-          _type = column.int(1) # enum_field_types, I'll need a bunch of this for casting
-          column.int(2)
-          column.int(1)
-          fields << name
-        end
-      end
-
-      more_rows = true
-      while more_rows
-        @sock.read_packet do |row|
-          if row.eof?
-            more_rows = false
-            break
-          end
-
-          rows << column_count.times.map do
-            # TODO casting based on column details
-            row.nil_or_lenenc_str
-          end
-        end
-      end
-    end
-
-    Result.new(fields, rows)
+    Protocol::Query.new(@sock, @options).query(sql)
   end
 
   def ping
@@ -114,12 +66,5 @@ class Nocturne
     end
 
     @sock.close
-  end
-
-  private
-
-  def connect
-    connection = Protocol::Connection.new(@sock, @options).tap(&:establish)
-    @server_version = connection.server_version
   end
 end
---
+++ lib/nocturne/protocol/query.rb
@@ -0,0 +1,87 @@
+#frozen_string_literal: true
+
+class Nocturne
+  module Protocol
+    class Query
+      def initialize(sock, options)
+        @sock = sock
+        @options = options
+      end
+
+      def query(sql)
+        @sock.begin_command
+
+        @sock.write_packet do |packet|
+          packet.int(1, COM_QUERY)
+          packet.str(sql)
+        end
+
+        column_count = 0
+        @sock.read_packet do |payload|
+          if payload.ok?
+            # Done. No results.
+          else
+            column_count = payload.lenenc_int
+          end
+        end
+
+        fields = read_fields(column_count)
+        rows = read_rows(column_count)
+        Result.new(fields, rows)
+      end
+
+      private
+
+      def read_fields(column_count)
+        fields = []
+
+        column_count.times do
+          @sock.read_packet do |column|
+            column.lenenc_str
+            column.lenenc_str
+            column.lenenc_str
+            column.lenenc_str
+            column.lenenc_str
+            name = column.lenenc_str
+            column.lenenc_int
+            column.int(2)
+            column.int(4)
+            _type = column.int(1) # enum_field_types, I'll need a bunch of this for casting
+            column.int(2)
+            column.int(1)
+            fields << name
+          end
+        end
+
+        fields
+      end
+
+      def read_rows(column_count)
+        return [] if column_count.zero?
+
+        rows = []
+
+        more_rows = true
+        while more_rows
+          @sock.read_packet do |row|
+            if row.eof?
+              more_rows = false
+              break
+            end
+
+            rows << read_row(column_count, row)
+          end
+        end
+
+        rows
+      end
+
+      def read_row(column_count, row)
+        column_count.times.map do
+          # TODO casting based on column details
+          row.nil_or_lenenc_str
+        end
+      end
+    end
+  end
+end
