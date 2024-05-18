f721fa52d12c4410680a6b593dccc95546ab44eb
Daniel Colson <danieljamescolson@gmail.com>
2024-05-18 09:34:46 -0400

    Improve escaping
    This commit speeds up escaping by avoiding method calls and hash lookups
    inside the loop.
    
    I've tried various things here, much of it suggested by Matthew and
    John at RailsConf.
    
    I think this can still be improved, but I probably need to decide which
    cases to optimize for. I'm guessing that most strings won't require much
    escaping, so maybe we can optimize for that (e.g. using index in the
    NO_BACKSLASH_ESCAPES version works well for strings that don't need
    escaping but gets fairly slow if we have to call it multiple times).
    
    Co-authored-by: John Hawthorn <jhawthorn@github.com>
    Co-authored-by: Matthew Draper <matthewd@github.com>

--- lib/nocturne/escaping.rb
+++ lib/nocturne/escaping.rb
@@ -2,45 +2,44 @@
 
 class Nocturne
   module Escaping
-    ESCAPES = {
-      "\"".ord => "\"",
-      "\0".ord => "0",
-      "'".ord => "'",
-      "\\".ord => "\\",
-      "\n".ord => "n",
-      "\r".ord => "r",
-      "\x1A".ord => "Z"
-    }
-    SINGLE_QUOTE = "'".ord
-
     def escape(str)
       encoding = str.encoding
 
-      if !encoding.ascii_compatible?
+      unless encoding.ascii_compatible?
         raise Encoding::CompatibilityError, "input string must be ASCII-compatible"
       end
 
       res = str.dup
-      pos = 0
 
-      i = 0
-      j = str.length
-      while i < j
-        byte = str.getbyte(i)
-
-        if !@conn.status_flag?(Protocol::SERVER_STATUS_NO_BACKSLASH_ESCAPES)
-          if (escaped = ESCAPES[byte])
-            res.bytesplice(pos, 0, "\\")
-            pos += 1
-            res[pos] = escaped
-          end
-        elsif byte == SINGLE_QUOTE
-          res.bytesplice(pos, 0, "'")
-          pos += 1
+      if @conn.status_flag?(Protocol::SERVER_STATUS_NO_BACKSLASH_ESCAPES)
+        idx = 0
+        while idx = res.index("'", idx)
+          res.bytesplice(idx, 0, "'")
+          idx += 2
         end
+      else
+        i = 0
+        len = str.length
+        while i < len
+          byte = res.getbyte(i)
 
-        i += 1
-        pos += 1
+          escaped = case byte
+                    when 0 then "\\0"
+                    when 10 then "\\n"
+                    when 13 then "\\r"
+                    when 26 then "\\Z"
+                    when 34 then "\\\""
+                    when 39 then "\\'"
+                    when 92 then "\\\\"
+                    else
+                      i += 1
+                      next
+                    end
+
+          res.bytesplice(i, 1, escaped)
+          i += 2
+          len += 1
+        end
       end
 
       res
